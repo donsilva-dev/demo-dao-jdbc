d197ee8b782c7a078f989259372037533ce6253e
Douglas nunes <donsilva-dev>
2024-07-11 23:16:11 -0300

    findByDepartment implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,17 +2,26 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
-	/* Interface DAO: Define as operações que podem ser realizadas nos dados, 
-	 * como inserir, atualizar, deletar e buscar. Essa interface é 
-	 * implementada por classes concretas que contêm o 
-	 * código específico para interagir com a base de dados.*/
+	/*
+	 * Interface DAO: Define as operações que podem ser realizadas nos dados, como
+	 * inserir, atualizar, deletar e buscar. Essa interface é implementada por
+	 * classes concretas que contêm o código específico para interagir com a base de
+	 * dados.
+	 */
 
 	void insert(SellerDao obj);
+
 	void update(SellerDao obj);
+
 	void deleteById(Integer id);
+
 	Seller findById(Integer id);
-	List<SellerDao>findAll();
+
+	List<Seller> findAll();
+
+	List<Seller> findByIdDepartment(Department department);
 }
