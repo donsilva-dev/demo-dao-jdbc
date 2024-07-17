fc2bfd3dfee9d3bc3949f297a66615083db11f8e
Douglas nunes <donsilva-dev>
2024-07-17 13:46:15 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,13 @@ public class Program {
 			
 		}
 
+		System.out.println(" \n==== Teste 3: seller findAll ====");
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+			
+		}
 	}
 
 }
